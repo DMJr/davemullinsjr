f15751143adff51603e81dcd96c628478e8e6caf
Dave Mullins <davemullinsjr@gmail.com>
2016-10-01 21:24:49 -0400

    add_styling_lib added images to examples page

--- app/routes/examples.js
+++ app/routes/examples.js
@@ -4,7 +4,7 @@ let websites = [{
   id: 'hapyak',
   title: 'Hapyak Interactive Video',
   date: 'October 2014 - Current',
-  image: '',
+  image: '//corp.hapyak.com/img/hy-nav-logo.png',
   description: "HapYak is an interactive video company based on Mozilla's interactive library, Popcorn.js.",
   languages: 'JavaScript, Python',
   frameworks: 'Ember, Flask, Node'
@@ -12,21 +12,21 @@ let websites = [{
   id: 'liftgateme',
   title: 'LiftGateMe',
   date: 'July 2009 - December 2015',
-  image: '',
+  image: '/assets/img/liftgateme.png',
   description: 'What started as a challenge to myself during my freshman year of college, LiftGateMe ultimately inspired my career as a developer and entreprenuer.  With an average monthly revenue of over $30,000 LiftGateMe serves customers accross America utilizing automated processes.',
   languages: 'PHP, JavaScript'
 }, {
   id: 'kwickfind',
   title: 'KwickFind',
   date: '2016',
-  image: '',
+  image: '/assets/img/kwickfind.png',
   description: 'KwickFind is a lips orem. ... FIX THIS',
   languages: 'FIX THIS'
 }, {
   id: 'eagleliftgate',
   title: 'EagleLiftGate',
   date: '',
-  image: '',
+  image: '/assets/img/eagleliftgate.png',
   description: 'EagleLiftGate FIX THIS',
   languages: 'FIX THIS',
   frameworks: 'FIX THIS'
